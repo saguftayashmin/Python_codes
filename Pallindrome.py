2e39d465c4f4cd5492920a6ed74c07ff7318daf1
saguftayashmin <77384375+saguftayashmin@users.noreply.github.com>
2025-10-11 23:35:18 +0530

    Update Pallindrome.py

--- Pallindrome.py
+++ Pallindrome.py
@@ -1,1 +1,10 @@
+#using slicing concept
+def pallincheck(x):
+    if(x==x[::-1]):
+        print("it a pallindrome")
+    else:
+        print("not pallindrome")
+        
+x= "12321"
+pallincheck(x)
 
