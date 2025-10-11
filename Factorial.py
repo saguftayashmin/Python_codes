f5419752c412a9c16657fe6bb7987cac2cdb639b
saguftayashmin <77384375+saguftayashmin@users.noreply.github.com>
2025-10-12 00:29:16 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,3 +1,4 @@
+# When a function calling itself again and again till it met any condition is called recursion
 def factorial(n):
     if n == 0:
         return 1
@@ -9,5 +10,13 @@ number = 5
 result = factorial(number)
 print(f"The factorial of {number} is {result}")
 
+#normal using loop
+def factorial_iterative(n):
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial = factorial * i
+    return factorial
 
-# When a function calling itself again and again till it met any condition is called recursion
+# Example usage
+number = 3
+print(f"The factorial of {number} is {factorial_iterative(number)}")
