3e441e3f8300b08979a1505707cd0db81f80b991
saguftayashmin <77384375+saguftayashmin@users.noreply.github.com>
2025-10-14 19:48:35 +0530

    Update commonelement.py

--- commonelement.py
+++ commonelement.py
@@ -8,3 +8,15 @@ for i in list_a:
     if i in list_b:
         common_elem.append(i)
 print(common_elem)
+
+#Write a Python program to remove duplicates from a list.
+nums = [1, 2, 3, 2, 1, 3, 2, 4, 5, 4]
+
+#print(list(set(nums)))
+
+uniq = []
+
+for i in nums:
+    if i not in uniq:
+        uniq.append(i)
+print(uniq)
